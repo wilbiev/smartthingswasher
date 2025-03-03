1bc9f8dd561936a9b4b94ff531529fde21778374
wilbiev <vilsteren@kpnmail.nl>
2025-03-03 20:30:10 +0100

    Add multiple switch support

--- custom_components/smartthingswasher/switch.py
+++ custom_components/smartthingswasher/switch.py
@@ -4,13 +4,18 @@ from __future__ import annotations
 
 from typing import Any
 
-from pysmartthings import Attribute, Capability, Command
+from pysmartthings import Attribute, Capability, Command, SmartThings
 
-from homeassistant.components.switch import SwitchEntity
+from homeassistant.components.switch import (
+    SwitchDeviceClass,
+    SwitchEntity,
+    SwitchEntityDescription,
+)
 from homeassistant.core import HomeAssistant
+from homeassistant.const import EntityCategory
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
-from . import SmartThingsConfigEntry
+from . import FullDevice, SmartThingsConfigEntry
 from .const import MAIN
 from .entity import SmartThingsEntity
 
@@ -28,6 +33,65 @@ AC_CAPABILITIES = (
     Capability.THERMOSTAT_COOLING_SETPOINT,
 )
 
+class SmartThingsSwitch(SmartThingsEntity, SwitchEntity):
+    """Describe a SmartThings switch entity."""
+
+    entity_description: SwitchEntityDescription
+
+    def __init__(
+        self,
+        client: SmartThings,
+        device: FullDevice,
+        entity_description: SwitchEntityDescription,
+        capability: Capability,
+        attribute: Attribute,
+    ) -> None:
+        """Init the class."""
+        super().__init__(client, device, {capability})
+        self._attr_unique_id = f"{device.device.device_id}{entity_description.unique_id_separator}{entity_description.key}"
+        self._attribute = attribute
+        self.capability = capability
+        self.entity_description = entity_description
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if switch is on."""
+        return self.device.status.get(self.entity_description.key, False)
+
+    def turn_on(self, **kwargs: Any) -> None:
+        """Turn the switch on."""
+        self._send_command([{"code": self.entity_description.key, "value": True}])
+
+    def turn_off(self, **kwargs: Any) -> None:
+        """Turn the switch off."""
+        self._send_command([{"code": self.entity_description.key, "value": False}])
+
+
+CAPABILITY_TO_SWITCHES: dict[
+    Capability, dict[Attribute, list[SwitchEntityDescription]]
+] = {
+    Capability.SWITCH: {
+        Attribute.SWITCH: [
+            SwitchEntityDescription(
+                key=Attribute.SWITCH,
+                translation_key="switch",
+                device_class=SwitchDeviceClass.SWITCH,
+                entity_category=EntityCategory.CONFIG,
+            )
+        ]
+    },
+    Capability.SAMSUNG_CE_WASHER_BUBBLE_SOAK: {
+        Attribute.STATUS: [
+            SwitchEntityDescription(
+                key="bubblesoak_switch",
+                translation_key="bubblesoak",
+                device_class=SwitchDeviceClass.SWITCH,
+                entity_category=EntityCategory.CONFIG,
+            )
+        ]
+    },
+}
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -37,11 +101,14 @@ async def async_setup_entry(
     """Add switches for a config entry."""
     entry_data = entry.runtime_data
     async_add_entities(
-        SmartThingsSwitch(entry_data.client, device, {Capability.SWITCH})
+        SmartThingsSwitch(entry_data.client, device, description, capability, attribute)
         for device in entry_data.devices.values()
-        if Capability.SWITCH in device.status[MAIN]
+        for capability, attributes in CAPABILITY_TO_SWITCHES.items()
+        if capability in device.status[MAIN]
         and not any(capability in device.status[MAIN] for capability in CAPABILITIES)
         and not all(capability in device.status[MAIN] for capability in AC_CAPABILITIES)
+        for attribute, descriptions in attributes.items()
+        for description in descriptions
     )
 
 
@@ -50,46 +117,37 @@ class SmartThingsSwitch(SmartThingsEntity, SwitchEntity):
 
     _attr_name = None
 
+    def __init__(
+        self,
+        client: SmartThings,
+        device: FullDevice,
+        entity_description: SwitchEntityDescription,
+        capability: Capability,
+        attribute: Attribute,
+    ) -> None:
+        """Init the class."""
+        super().__init__(client, device, {capability})
+        self._attr_unique_id = f"{device.device.device_id}{entity_description.unique_id_separator}{entity_description.key}"
+        self._attribute = attribute
+        self.capability = capability
+        self.entity_description = entity_description
+
+
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the switch off."""
         await self.execute_device_command(
-            Capability.SWITCH,
+            self.capability,
             Command.OFF,
         )
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the switch on."""
         await self.execute_device_command(
-            Capability.SWITCH,
+            self.capability,
             Command.ON,
         )
 
     @property
     def is_on(self) -> bool:
         """Return true if switch is on."""
-        return self.get_attribute_value(Capability.SWITCH, Attribute.SWITCH) == "on"
-
-
-class SmartThingsBubbleSoak(SmartThingsEntity, SwitchEntity):
-    """Define a SmartThings BubbleSoak switch."""
-
-    _attr_name = None
-
-    async def async_turn_off(self, **kwargs: Any) -> None:
-        """Turn the switch off."""
-        await self.execute_device_command(
-            Capability.SAMSUNG_CE_WASHER_BUBBLE_SOAK,
-            Command.OFF,
-        )
-
-    async def async_turn_on(self, **kwargs: Any) -> None:
-        """Turn the switch on."""
-        await self.execute_device_command(
-            Capability.SAMSUNG_CE_WASHER_BUBBLE_SOAK,
-            Command.ON,
-        )
-
-    @property
-    def is_on(self) -> bool:
-        """Return true if switch is on."""
-        return self.get_attribute_value(Capability.SAMSUNG_CE_WASHER_BUBBLE_SOAK, Attribute.STATUS) == "on"
+        return self.get_attribute_value(self.capability, self._attribute) == "on"
